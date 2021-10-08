a75282960f989e2f9378c73697ba3bdbb741429d
Jenya705 <jenya.ushenin@gmail.com>
2021-10-09 02:26:14 +0300

    fixed starting

--- mcapi-server-core/src/main/java/com/github/jenya705/mcapi/module/config/ConfigModuleImpl.java
+++ mcapi-server-core/src/main/java/com/github/jenya705/mcapi/module/config/ConfigModuleImpl.java
@@ -33,7 +33,7 @@ public class ConfigModuleImpl extends AbstractApplicationModule implements Confi
     private final CacheClassMap<ObjectTunnelFunction<?, Object>> serializers = new CacheClassMap<>();
 
     @SuppressWarnings("unchecked")
-    @OnInitializing(priority = 0)
+    @OnInitializing(priority = 1)
     public void initialize() throws IOException {
         this
                 .raw(String.class)
